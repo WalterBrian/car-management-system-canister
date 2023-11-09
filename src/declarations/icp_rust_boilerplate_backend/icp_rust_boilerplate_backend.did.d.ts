dd9e14093100652df4f0ebbd7902697449ff7bc2
Walter Brian <133803737+WalterBrian@users.noreply.github.com>
2023-11-08 20:03:35 -0500

    patch_fix

--- src/declarations/icp_rust_boilerplate_backend/icp_rust_boilerplate_backend.did.d.ts
+++ src/declarations/icp_rust_boilerplate_backend/icp_rust_boilerplate_backend.did.d.ts
@@ -1,34 +1,92 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
+/**
+ * Represents a car object.
+ */
 export interface Car {
-  'id' : bigint,
-  'is_booked' : boolean,
-  'model' : string,
-  'updated_at' : [] | [bigint],
-  'owner' : string,
-  'make' : string,
-  'color' : string,
-  'year' : number,
-  'created_at' : bigint,
+  'id': bigint,
+  'is_booked': boolean,
+  'model': string,
+  'updated_at': bigint | undefined,
+  'owner': string,
+  'make': string,
+  'color': string,
+  'year': number,
+  'created_at': bigint,
 }
-export interface CarPayload {
-  'is_booked' : boolean,
-  'model' : string,
-  'owner' : string,
-  'make' : string,
-  'color' : string,
-  'year' : number,
+
+/**
+ * Represents the payload for updating a car.
+ */
+export interface CarUpdatePayload {
+  'is_booked': boolean,
+  'model': string,
+  'owner': string,
+  'make': string,
+  'color': string,
+  'year': number,
 }
-export type Error = { 'NotFound' : { 'msg' : string } };
-export type Result = { 'Ok' : Car } |
-  { 'Err' : Error };
-export type Result_1 = { 'Ok' : boolean } |
-  { 'Err' : Error };
+
+/**
+ * Represents an error type.
+ */
+export type Error = { 'NotFound': { 'msg': string } };
+
+/**
+ * Represents a result that can contain a car object or an error.
+ */
+export type Result = { 'Ok': Car } | { 'Err': Error };
+
+/**
+ * Represents a result that can contain a boolean value or an error.
+ */
+export type BooleanResult = { 'Ok': boolean } | { 'Err': Error };
+
+/**
+ * Service interface with actor methods.
+ */
 export interface _SERVICE {
-  'add_car' : ActorMethod<[CarPayload], [] | [Car]>,
-  'delete_car' : ActorMethod<[bigint], Result>,
-  'get_car' : ActorMethod<[bigint], Result>,
-  'is_booked' : ActorMethod<[bigint], Result_1>,
-  'update_car' : ActorMethod<[bigint, CarPayload], Result>,
+  'add_car': ActorMethod<[CarUpdatePayload], [] | [Car]>,
+  'delete_car': ActorMethod<[bigint], Result>,
+  'get_car': ActorMethod<[bigint], Result>,
+  'is_booked': ActorMethod<[bigint], BooleanResult>,
+  'update_car': ActorMethod<[bigint, CarUpdatePayload], Result>,
+}
+
+/**
+ * Validate the CarUpdatePayload before updating a car.
+ */
+function validateCarUpdatePayload(payload: CarUpdatePayload): boolean {
+  if (!payload.is_booked || !payload.model || !payload.owner || !payload.make || !payload.color || !payload.year) {
+    throw new Error('CarUpdatePayload is missing required fields.');
+  }
+
+  // Additional validation logic can be added here as needed.
+
+  return true;
+}
+
+/**
+ * Handle errors gracefully and wrap results in a standardized format.
+ */
+async function handleActorMethod<ResultType>(
+  method: ActorMethod<any, ResultType>,
+  ...params: any[]
+): Promise<ResultType> {
+  try {
+    const result = await method(...params);
+    if ('Err' in result) {
+      throw new Error(result.Err.msg);
+    }
+    return result.Ok;
+  } catch (error) {
+    throw new Error(`Error in actor method: ${error.message}`);
+  }
+}
+
+// Example usage of input validation and error handling in actor methods:
+_SERVICE.update_car = async (id, payload) => {
+  validateCarUpdatePayload(payload);
+  return handleActorMethod(_SERVICE.update_car, id, payload);
 }
