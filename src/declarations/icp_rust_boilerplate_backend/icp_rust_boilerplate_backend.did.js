1bffa52daca5a0a98359c2dc7646539e281aa411
Walter Brian <133803737+WalterBrian@users.noreply.github.com>
2023-11-08 20:15:30 -0500

    patch-complete

--- src/declarations/icp_rust_boilerplate_backend/icp_rust_boilerplate_backend.did.js
+++ src/declarations/icp_rust_boilerplate_backend/icp_rust_boilerplate_backend.did.js
@@ -1,32 +1,91 @@
+/**
+ * IDL Factory for the Car Service
+ *
+ * This IDL factory defines the service and types for a Car Service, which can be used
+ * in the Internet Computer (IC) environment.
+ *
+ * @param {object} { IDL } - An object containing IDL namespace.
+ * @returns {object} - An IDL service definition.
+ */
 export const idlFactory = ({ IDL }) => {
+  // Define a record type representing the payload for a car.
   const CarPayload = IDL.Record({
-    'is_booked' : IDL.Bool,
-    'model' : IDL.Text,
-    'owner' : IDL.Text,
-    'make' : IDL.Text,
-    'color' : IDL.Text,
-    'year' : IDL.Nat32,
+    'is_booked': IDL.Bool,   // Indicates whether the car is booked.
+    'model': IDL.Text,      // The car's model name.
+    'owner': IDL.Text,      // The owner of the car.
+    'make': IDL.Text,       // The car's make (brand).
+    'color': IDL.Text,      // The car's color.
+    'year': IDL.Nat32,      // The manufacturing year of the car.
   });
+
+  // Define a record type representing a car object.
   const Car = IDL.Record({
-    'id' : IDL.Nat64,
-    'is_booked' : IDL.Bool,
-    'model' : IDL.Text,
-    'updated_at' : IDL.Opt(IDL.Nat64),
-    'owner' : IDL.Text,
-    'make' : IDL.Text,
-    'color' : IDL.Text,
-    'year' : IDL.Nat32,
-    'created_at' : IDL.Nat64,
+    'id': IDL.Nat64,            // Unique identifier for the car.
+    'is_booked': IDL.Bool,       // Indicates whether the car is booked.
+    'model': IDL.Text,          // The car's model name.
+    'updated_at': IDL.Opt(IDL.Nat64), // Optional field for update timestamp.
+    'owner': IDL.Text,          // The owner of the car.
+    'make': IDL.Text,           // The car's make (brand).
+    'color': IDL.Text,          // The car's color.
+    'year': IDL.Nat32,          // The manufacturing year of the car.
+    'created_at': IDL.Nat64,    // Timestamp when the car was created.
   });
-  const Error = IDL.Variant({ 'NotFound' : IDL.Record({ 'msg' : IDL.Text }) });
-  const Result = IDL.Variant({ 'Ok' : Car, 'Err' : Error });
-  const Result_1 = IDL.Variant({ 'Ok' : IDL.Bool, 'Err' : Error });
+
+  // Define an error variant with error code and message for more detailed error handling.
+  const Error = IDL.Variant({
+    'NotFound': IDL.Record({
+      'code': IDL.Nat32,  // Error code indicating 'not found.'
+      'msg': IDL.Text,    // Error message providing more details.
+    }),
+  });
+
+  // Define a result variant that can contain a car object or an error.
+  const Result = IDL.Variant({ 'Ok': Car, 'Err': Error });
+
+  // Define another result variant that can contain a boolean value or an error.
+  const Result_1 = IDL.Variant({ 'Ok': IDL.Bool, 'Err': Error });
+
+  // Define the service with actor methods.
   return IDL.Service({
-    'add_car' : IDL.Func([CarPayload], [IDL.Opt(Car)], []),
-    'delete_car' : IDL.Func([IDL.Nat64], [Result], []),
-    'get_car' : IDL.Func([IDL.Nat64], [Result], ['query']),
-    'is_booked' : IDL.Func([IDL.Nat64], [Result_1], ['query']),
-    'update_car' : IDL.Func([IDL.Nat64, CarPayload], [Result], []),
+    /**
+     * Adds a new car to the system.
+     *
+     * @param {CarPayload} payload - Data of the car to be added.
+     * @returns {Car | null} - The added car or null in case of an error.
+     */
+    'add_car': IDL.Func([CarPayload], [IDL.Opt(Car)], []),
+
+    /**
+     * Deletes a car by its unique ID.
+     *
+     * @param {bigint} id - The unique identifier of the car to be deleted.
+     * @returns {Result} - Contains either the deleted car or an error.
+     */
+    'delete_car': IDL.Func([IDL.Nat64], [Result], []),
+
+    /**
+     * Retrieves a car by its unique ID (query method).
+     *
+     * @param {bigint} id - The unique identifier of the car to be retrieved.
+     * @returns {Result} - Contains either the retrieved car or an error.
+     */
+    'get_car': IDL.Func([IDL.Nat64], [Result], ['query']),
+
+    /**
+     * Checks if a car is booked (query method).
+     *
+     * @param {bigint} id - The unique identifier of the car to be checked.
+     * @returns {Result_1} - Contains either a boolean value or an error.
+     */
+    'is_booked': IDL.Func([IDL.Nat64], [Result_1], ['query']),
+
+    /**
+     * Updates car data.
+     *
+     * @param {bigint} id - The unique identifier of the car to be updated.
+     * @param {CarPayload} payload - New data for the car.
+     * @returns {Result} - Contains either the updated car or an error.
+     */
+    'update_car': IDL.Func([IDL.Nat64, CarPayload], [Result], []),
   });
 };
-export const init = ({ IDL }) => { return []; };
--- src/declarations/icp_rust_boilerplate_backend/index.js
+++ src/declarations/icp_rust_boilerplate_backend/index.js
@@ -1,19 +1,41 @@
 import { Actor, HttpAgent } from "@dfinity/agent";
-
-// Imports and re-exports candid interface
 import { idlFactory } from "./icp_rust_boilerplate_backend.did.js";
-export { idlFactory } from "./icp_rust_boilerplate_backend.did.js";
 
-/* CANISTER_ID is replaced by webpack based on node environment
- * Note: canister environment variable will be standardized as
- * process.env.CANISTER_ID_<CANISTER_NAME_UPPERCASE>
- * beginning in dfx 0.15.0
- */
-export const canisterId =
+// Re-export the idlFactory for others to use
+export { idlFactory };
+
+// Get the canisterId from environment variables
+const canisterId =
   process.env.CANISTER_ID_ICP_RUST_BOILERPLATE_BACKEND ||
   process.env.ICP_RUST_BOILERPLATE_BACKEND_CANISTER_ID;
 
+/**
+ * Create an actor with the provided canister ID and options.
+ * @param {string | Principal} canisterId - ID of the canister the Actor will communicate with.
+ * @param {object} options - Configuration options for actor creation.
+ * @param {Agent} options.agent - (Optional) Pre-configured agent to use.
+ * @param {object} options.agentOptions - (Optional) Options for setting up a new agent.
+ * @param {object} options.actorOptions - (Optional) Options for the Actor.
+ * @returns {Actor} - The created actor instance.
+ * @throws {Error} If canisterId is not provided or is invalid.
+ */
 export const createActor = (canisterId, options = {}) => {
+  // Validate the canisterId.
+  if (!canisterId) {
+    throw new Error("Canister ID is required.");
+  }
+
+  if (typeof canisterId === "string") {
+    try {
+      canisterId = Principal.fromText(canisterId);
+    } catch (error) {
+      throw new Error("Invalid canister ID format.");
+    }
+  } else if (!(canisterId instanceof Principal)) {
+    throw new Error("Invalid canister ID type.");
+  }
+
+  // Create the HttpAgent with options.
   const agent = options.agent || new HttpAgent({ ...options.agentOptions });
 
   if (options.agent && options.agentOptions) {
@@ -22,17 +44,17 @@ export const createActor = (canisterId, options = {}) => {
     );
   }
 
-  // Fetch root key for certificate validation during development
+  // Fetch the root key for certificate validation during development
   if (process.env.DFX_NETWORK !== "ic") {
     agent.fetchRootKey().catch((err) => {
       console.warn(
-        "Unable to fetch root key. Check to ensure that your local replica is running"
+        "Unable to fetch the root key. Check to ensure that your local replica is running."
       );
       console.error(err);
     });
   }
 
-  // Creates an actor with using the candid interface and the HttpAgent
+  // Create an actor using the candid interface and the HttpAgent
   return Actor.createActor(idlFactory, {
     agent,
     canisterId,
@@ -40,4 +62,5 @@ export const createActor = (canisterId, options = {}) => {
   });
 };
 
+// Export the initialized actor for use in other modules.
 export const icp_rust_boilerplate_backend = createActor(canisterId);
